19a4984ce9b3d1cbc220591037333c990442fd11
i517819 <sean.yu@sap.com>
2020-08-13 18:14:15 +0800

    fix: 修复了碰撞检测逻辑，增加了水平相交检测

--- src/main/java/sean/yu/Tetris.java
+++ src/main/java/sean/yu/Tetris.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 /**
  * 定义了一组下落的方块
- *
+ * <p>
  * 每组方块可以看成由多个小正方形方块（tList）无缝拼接而成
  *
  * @author Sean Yu
@@ -92,10 +92,14 @@ public class Tetris {
         //先判断和底部方块是是否有相交
         for (Rectangle currRect : t.rList) {
             for (Rectangle bottomRect : t.frame.bottomRectList) {
-                int currDis = bottomRect.y - (currRect.y + SIDE_LEN);
-                //如果底部到当前方块的垂直距离小于正常fall一次的距离（即速度），则更新本次下落的距离
-                if (currDis < SPEED) {
-                    minDis = Math.min(minDis, currDis);
+                //先判断当前方块是否在底部方块的上方
+                if (isHorizontalIntersect(currRect, bottomRect)) {
+                    //如果是，再计算下落到底部方块的距离
+                    int currDis = bottomRect.y - (currRect.y + SIDE_LEN);
+                    //如果底部到当前方块的垂直距离小于正常fall一次的距离（即速度），则更新本次下落的距离
+                    if (currDis < SPEED) {
+                        minDis = Math.min(minDis, currDis);
+                    }
                 }
             }
         }
@@ -112,5 +116,27 @@ public class Tetris {
             }
         }
         return minDis;
+    }
+
+    /**
+     * 判断2个正方形在y相同时，是否在水平部分有重合
+     * <p>
+     * 判断方法：
+     * 将当前下落正方形的水平方向的一条边记为Lc : (xc1,xc2)
+     * 将bottom正方形的水平方向的一条边记为Lb : (xb1,xb2)
+     * 则Lc 和 Lb 不相交的条件是： Lc在Lb的左侧（xc2 <= xb1）或右侧 (xc1 >= xb2)
+     * 则相交的条件为 ： 不相交条件取反
+     *
+     * @param curr
+     * @param bottom
+     * @return
+     */
+    private boolean isHorizontalIntersect(Rectangle curr, Rectangle bottom) {
+        int xc1 = curr.x;
+        int xc2 = curr.x + SIDE_LEN;
+        int xb1 = bottom.x;
+        int xb2 = bottom.x + SIDE_LEN;
+        boolean isNotIntersact = (xc2 <= xb1) || (xc1 >= xb2);
+        return !isNotIntersact;
     }
 }
